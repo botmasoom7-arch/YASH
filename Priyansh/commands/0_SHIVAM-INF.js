5320b39f3981f123a469038a18c14092aeeaea62
tirkumar58-sketch <tirkumar58@gmail.com>
2026-02-07 10:47:29 +0530

    Add files via upload

---
+++ Priyansh/commands/0_SHIVAM-INF.js
@@ -0,0 +1,32 @@
+module.exports.config = {
+  name: "inf",
+  version: "1.0.1", 
+  hasPermssion: 0,
+  credits:"SHIVAM SAINI" ,
+  description: "THIS BOT IS MADE BY SHIVAM SAINI",
+  usePrefix: true,
+  commandCategory: "INFORMATION",
+  cooldowns: 1,
+  dependencies: 
+  {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+    hours = Math.floor(time / (60 * 60)),
+    minutes = Math.floor((time % (60 * 60)) / 60),
+    seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Kolkata").format("DD/MM/YYYY");
+var link =                                     
+["https://i.imgur.com/kIN7j9r.jpeg","https://i.imgur.com/kIN7j9r.jpeg","https://i.imgur.com/kIN7j9r.jpeg"];
+var callback = () => api.sendMessage({body:`┏━━━━━┓\n     SHIVAM-SAINI                    ✧═══•❁🙊❁•═══✧\n┗━━━━━┛\n\n\n         ❁ ═════════ ❃•❃ ═════════ ❁\n                            ♥️ 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡 ♥️\n         ❁ ═════════ ❃•❃ ═════════ ❁\n\n\n\n🌺  [ 1 ]  𒁍 BOT NAME   →   ${global.config.BOTNAME}\n🌺  [ 2 ]  𒁍 BOT ADMIN   →   SHIVAM-SAINI\n🌺  [ 3 ]  𒁍 BOT  PREFIX   →   [ ${global.config.PREFIX} ]\n🌺  [ 4 ]  𒁍  DATE   → ${juswa}\n🌺  [ 5 ]  𒁍  BOT RUNNING TIME  →  [ ${hours}:${minutes}:${seconds} ]\n🌺  [ 6 ]  𒁍  https://www.facebook.com/shivamssxd001\n🌺  [ 7 ]  𒁍 WHATSAPP → [ +91 9149xxxx53 ]\n🌺  [ 8 ]  𒁍 INSTAGRAM →  SAINI_UK07\n🌺  [ 9 ]  𒁍 YOUTUBE  →  SHIVAM SAINI XD\n🌺  [ 10 ]  𒁍 GITHUB  →  @SAINI - 001\n🌺THANKYOU FOR USING ${global.config.BOTNAME} BOT♥️\n\n\n
+`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
+   };
